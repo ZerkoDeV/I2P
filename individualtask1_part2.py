e5c439ab34688dac0ac1c2e8b0d9c8aa31bba97c
ZerkoDeV <mikhael.a.salim@gmail.com>
2019-11-20 11:57:44 +0700

    Individual Task

---
+++ individualtask1_part2.py
@@ -0,0 +1,52 @@
+degrees = 150
+radians = (degrees * (3.14/180))
+
+print("Degrees:" + str(degrees))
+print("Radians:" + str(radians))
+
+student1 = 80.0
+student2 = 90.0
+student3 = 65.5
+average = (student1+student2+student3)/3
+print("Student scores:")
+print(student1)
+print(student2)
+print(student3)
+print("Average:" + str(average))
+
+class1 = 32
+class2 = 45
+class3 = 51
+
+class_group1 = (class1 // 5)
+class_group2 = (class2 // 7)
+class_group3 = (class3 // 6)
+leftover1 = (class1 % 5)
+leftover2 = (class2 % 7)
+leftover3 = (class3 % 6)
+
+print("Number of student in each group.")
+print("Class 1 :" + str(class_group1))
+print("Class 2 :" + str(class_group2))
+print("Class 3 :" + str(class_group3))
+
+print("Number of students leftover.")
+print("Class 1 :" + str(leftover1))
+print("Class 2 :" + str(leftover2))
+print("Class 3 :" + str(leftover3))
+
+pi = 3.14
+pie_diameter = 55.4
+pie_radius = (pie_diameter / 2)
+circumference = (2 * pi * pie_radius)
+circumference_msg = ("Jimmys’ pie has a circumference: ")
+print(circumference_msg, circumference)
+
+wavelength = 0
+velocity = 343
+frequency = 256
+wavelength = velocity / frequency
+print("The speed (m/s) : " + str(velocity))
+print("The frequency (Hz) : " + str(frequency))
+print("The wavelength (m) : " + str(wavelength))
+
